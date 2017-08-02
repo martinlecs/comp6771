d55b753da1f57d31fa00eb283fe4494cce4d7599
Martin Le <martinle.cse@gmail.com>
2017-08-03 00:33:35 +1000

    implemented basic functions

--- ass1/calculator.cpp
+++ ass1/calculator.cpp
@@ -41,40 +41,126 @@ int main(int argc, char* argv[]) {
                     {
                         double i = std::stod(num1);
                         double j = std::stod(num2);
+                        double sum = i + j;
+                        std::string res = std::to_string(sum);
+                        st.push(res);
 
-                        std::cout << i + j << std::endl;
+                        std::cout << i << " + " << j << " = " 
+                            << sum << std::endl;
                     }
                     else {
                         int i = std::stoi(num1);
                         int j = std::stoi(num2);
-                        std::cout<< i + j << std::endl;
+                        int sum = i + j;
+                        std::string res = std::to_string(sum);
+                        st.push(res);
+
+                        std::cout << i << " + " << j << " = " 
+                            << sum << std::endl;
                     }
-/*
-                    int i, j;
-
-                    std::string num1 = st.top();
-
-                    if(s.find('.') != std::string::npos) {
-                        static_cast<double>(i);// = pop_convert_double(st);
-                    } else {
-                        i = pop_convert_int(st);
-                    }
-                    st.pop();
-
-                    std::string num2 = st.top();
-
-                    if(s.find('.') != std::string::npos) {
-                        const_cast<double>(j); 
-                        j = pop_convert_double(st);
-                    } else {
-                        j = pop_convert_int(st);
-                    }
-                    st.pop();
-
-                    std::cout << i + j << std::endl;
-*/
                 }
             }
+            
+            if (s == "sub") {
+                  if(st.size() >= 2) {
+                    std::string num1 = st.top();
+                    st.pop();
+                    std::string num2 = st.top();
+                    st.pop();
+
+                    if(num1.find ('.') != std::string::npos || 
+                       num2.find ('.') != std::string::npos )
+                    {
+                        double i = std::stod(num1);
+                        double j = std::stod(num2);
+                        double sub = i - j;
+                        std::string res = std::to_string(sub);
+                        st.push(res);
+
+                        std::cout << i << " - " << j << " = " 
+                            << sub << std::endl;
+                    }
+                    else {
+                        int i = std::stoi(num1);
+                        int j = std::stoi(num2);
+                        int sub = i - j;
+                        std::string res = std::to_string(sub);
+                        st.push(res);
+
+                        std::cout << i << " - " << j << " = " 
+                            << sub << std::endl;
+                    }
+                }
+            }
+          
+            
+            if (s == "mult") {
+                  if(st.size() >= 2) {
+                    std::string num1 = st.top();
+                    st.pop();
+                    std::string num2 = st.top();
+                    st.pop();
+
+                    if(num1.find ('.') != std::string::npos || 
+                       num2.find ('.') != std::string::npos )
+                    {
+                        double i = std::stod(num1);
+                        double j = std::stod(num2);
+                        double mul = i * j;
+                        std::string res = std::to_string(mul);
+                        st.push(res);
+
+                        std::cout << i << " * " << j << " = " 
+                            << mul << std::endl;
+                    }
+                    else {
+                        int i = std::stoi(num1);
+                        int j = std::stoi(num2);
+                        int mul = i * j;
+                        std::string res = std::to_string(mul);
+                        st.push(res);
+
+                        std::cout << i << " * " << j << " = " 
+                            << mul << std::endl;
+                    }
+                }
+            }
+           
+            if (s == "div") {
+                   if(st.size() >= 2) {
+                    std::string num1 = st.top();
+                    st.pop();
+                    std::string num2 = st.top();
+                    st.pop();
+
+                    if(num1.find ('.') != std::string::npos || 
+                       num2.find ('.') != std::string::npos )
+                    {
+                        double i = std::stod(num1);
+                        double j = std::stod(num2);
+                        double div = i / j;
+                        std::string res = std::to_string(div);
+                        st.push(res);
+
+                        std::cout << i << " / " << j << " = " 
+                            << div << std::endl;
+                    }
+                    else {
+                        int i = std::stoi(num1);
+                        int j = std::stoi(num2);
+                        int div = i / j;
+                        std::string res = std::to_string(div);
+                        st.push(res);
+
+                        std::cout << i << " / " << j << " = " 
+                            << div << std::endl;
+                    }
+                }           
+            }
+            if (s == "sqrt") {}
+            if (s == "pop") {}
+            if (s == "reverse") {}
+            if (s == "repeat") {}
         }
 	}
     
@@ -85,8 +171,8 @@ int main(int argc, char* argv[]) {
         auto t = st.top();
         std::cout << t << std::endl;
         st.pop();
-    }*/
-
+    }
+*/
 	in.close();
 }
 
