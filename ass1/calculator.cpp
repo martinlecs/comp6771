2e98e80838d9d735f393102e64770d46cfd17b00
Martin Le <martinle.cse@gmail.com>
2017-08-02 23:58:01 +1000

    implemented 'add' feature

--- ass1/calculator.cpp
+++ ass1/calculator.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <stack>
+
+double pop_convert_double (std::stack<std::string> &st);
+int pop_convert_int (std::stack<std::string> &st);
 
 int main(int argc, char* argv[]) {
 
@@ -15,9 +19,85 @@ int main(int argc, char* argv[]) {
 	// string to be read into
 	std::string s;
 
+    // create an empty stack
+    std::stack<std::string> st;
+
 	// read the file while we have input.
-	while (in >> s) {
-        std::cout << "hello\n";
+    while (in >> s) {
+        if(isdigit(s[0])) {
+            st.push(s);
+        }
+        else {
+            if(s == "add") {
+
+                if(st.size() >= 2) {
+                    std::string num1 = st.top();
+                    st.pop();
+                    std::string num2 = st.top();
+                    st.pop();
+
+                    if(num1.find ('.') != std::string::npos || 
+                       num2.find ('.') != std::string::npos )
+                    {
+                        double i = std::stod(num1);
+                        double j = std::stod(num2);
+
+                        std::cout << i + j << std::endl;
+                    }
+                    else {
+                        int i = std::stoi(num1);
+                        int j = std::stoi(num2);
+                        std::cout<< i + j << std::endl;
+                    }
+/*
+                    int i, j;
+
+                    std::string num1 = st.top();
+
+                    if(s.find('.') != std::string::npos) {
+                        static_cast<double>(i);// = pop_convert_double(st);
+                    } else {
+                        i = pop_convert_int(st);
+                    }
+                    st.pop();
+
+                    std::string num2 = st.top();
+
+                    if(s.find('.') != std::string::npos) {
+                        const_cast<double>(j); 
+                        j = pop_convert_double(st);
+                    } else {
+                        j = pop_convert_int(st);
+                    }
+                    st.pop();
+
+                    std::cout << i + j << std::endl;
+*/
+                }
+            }
+        }
 	}
+    
+    //DEBUGGING
+    //Print out contents of stack
+    /*
+    while(!st.empty()) {
+        auto t = st.top();
+        std::cout << t << std::endl;
+        st.pop();
+    }*/
+
 	in.close();
+}
+
+double pop_convert_double (std::stack<std::string> &st) {
+    double i = std::stod(st.top());
+    st.pop();
+    return i;
+}
+
+int pop_convert_int (std::stack<std::string> &st) {
+    int i = std::stoi(st.top());
+    st.pop();
+    return i;
 }
