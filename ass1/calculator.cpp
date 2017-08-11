ace24eccac40329cb38fe737720e664adc46aa6f
Martin Le <martinle.cse@gmail.com>
2017-08-11 21:26:00 +1000

    fixed up major problems and removed duplicate code. Need to redo nested loops

--- ass1/calculator.cpp
+++ ass1/calculator.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <sstream>
 #include <stack>
 #include <cmath>
-#include <queue>
 #include <vector>
+#include <queue>
 
 void addNumbers(std::stack<std::string>& st);
 void subNumbers(std::stack<std::string>& st);
@@ -13,11 +14,9 @@ void divNumbers(std::stack<std::string>& st);
 void sqrtNumber(std::stack<std::string>& st);
 void reverseStack(std::stack<std::string>& st);
 void printStack(std::stack<std::string>& st);
-void printQueue(std::queue<std::string>& qu);
 void processString(std::stack<std::string>& st, std::string &s);
 void processRepeats(std::ifstream &in, std::stack<std::string> &st, std::vector<std::string> &history);
-
-std::vector<std::string> loopVector;
+std::string modify_string_precision(std::string a_value);
 
 int main(int argc, char* argv[]) {
 
@@ -47,13 +46,17 @@ int main(int argc, char* argv[]) {
         }
     }
     in.close();
-    //printStack(st);
-    //printQueue(repqueue);
+    printStack(st);
 }
 
 void processString(std::stack<std::string>& st, std::string &s) {
-    if(isdigit(s[0])) {
-        st.push(s);
+    if(isdigit(s[0]) || isdigit(s[1])) {
+        if(s.find ('.') != std::string::npos) {
+            std::string converted = modify_string_precision(s);
+            st.push(converted);
+        } else {
+            st.push(s);
+        }
     }
     else {
         if (s == "add" && st.size() >= 2) {
@@ -74,7 +77,7 @@ void processString(std::stack<std::string>& st, std::string &s) {
         else if (s == "pop" && !st.empty()) {
             st.pop();
         }
-        else if (s == "reverse" && st.size() > 1) {
+        else if (s == "reverse" && !st.empty()) {
             reverseStack(st);
         }
         else {
@@ -86,7 +89,6 @@ void processString(std::stack<std::string>& st, std::string &s) {
 
 void processRepeats(std::ifstream &in, std::stack<std::string> &st, std::vector<std::string> &history) {
 
-    //Get number of repetitions
     int numRepeat = std::stoi(st.top());
     st.pop();
 
@@ -94,6 +96,10 @@ void processRepeats(std::ifstream &in, std::stack<std::string> &st, std::vector<
 
     while (in >> str) { //continue processing file stream
         if (str == "repeat") {
+
+            //For handling nested repeats. Pops the number of reapeats
+            //from the history. And then makes a recursive call to
+            //processRepeats
             st.push(history.back());
             history.pop_back();
             processRepeats(in, st, history);        
@@ -106,158 +112,168 @@ void processRepeats(std::ifstream &in, std::stack<std::string> &st, std::vector<
             if(!history.empty()) {
                 int i = 1;
 
-                //Copy history and append it to current history
-                 std::vector<std::string> copy = history;
+                std::vector<std::string> copy = history;
 
                 while (i < numRepeat) {
                     history.insert(std::end(history), std::begin(copy), std::end(copy));
                     ++i;
                 }
-                return;
             }
+            return;
         }   
     }
 }
 
 void addNumbers(std::stack<std::string>& st) {
+    
+    //Pop off the top two variables from the stack
     std::string num1 = st.top();
     st.pop();
     std::string num2 = st.top();
     st.pop();
 
-    if(num1.find ('.') != std::string::npos || 
-            num2.find ('.') != std::string::npos )
-    {
-        double i = std::stod(num1);
-        double j = std::stod(num2);
-        double sum = i + j;
-        std::string res = std::to_string(sum);
-        st.push(res);
+    //Create a variable to store the result
+    std::string result;
 
-        std::cout << num1 << " + " << num2 << " = " 
-            << sum << std::endl;
+    //Check if either of the numbers are doubles
+    if(num1.find ('.') != std::string::npos || num2.find ('.') != std::string::npos ) {
+        //Add the two numbers together as doubles and push the result onto the stack
+        result = std::to_string(std::stod(num1) + std::stod(num2));
+        result = modify_string_precision(result); //set precision of result 
     }
+    //otherwise the numbers must be ints
     else {
-        int i = std::stoi(num1);
-        int j = std::stoi(num2);
-        int sum = i + j;
-        std::string res = std::to_string(sum);
-        st.push(res);
-
-        std::cout << num1 << " + " << num2 << " = " 
-            << sum << std::endl;
+        result = std::to_string(std::stoi(num1) + std::stoi(num2));
     }
+    //Push result to stack
+    st.push(result);
+
+    //Print out the equation
+    std::cout << num1 << " + " << num2 << " = "
+        << result << std::endl;
 }
+
 void subNumbers(std::stack<std::string>& st) {
+    //Pop off the top two variables from the stack
     std::string num1 = st.top();
     st.pop();
     std::string num2 = st.top();
     st.pop();
 
-    if(num1.find ('.') != std::string::npos || 
-            num2.find ('.') != std::string::npos )
-    {
-        double i = std::stod(num1);
-        double j = std::stod(num2);
-        double sum = i - j;
-        std::string res = std::to_string(sum);
-        st.push(res);
+    //Create a variable to store the result
+    std::string result;
 
-        std::cout << num1 << " - " << num2 << " = " 
-            << sum << std::endl;
+    //Check if either of the numbers are doubles
+    if(num1.find ('.') != std::string::npos || num2.find ('.') != std::string::npos ) {
+        //Subtract the two numbers together as doubles and push the result onto the stack
+        result = std::to_string(std::stod(num1) - std::stod(num2));
+        result = modify_string_precision(result); //set precision of result 
     }
+    //otherwise the numbers must be ints
     else {
-        int i = std::stoi(num1);
-        int j = std::stoi(num2);
-        int sum = i - j;
-        std::string res = std::to_string(sum);
-        st.push(res);
-
-        std::cout << num1 << " - " << num2 << " = " 
-            << sum << std::endl;
+        result = std::to_string(std::stoi(num1) - std::stoi(num2));
     }
+    //Push result to stack
+    st.push(result);
+
+    //Print out the equation
+    std::cout << num1 << " - " << num2 << " = " 
+        << result << std::endl;
 }
 
 void multNumbers(std::stack<std::string>& st) {
+     //Pop off the top two variables from the stack
     std::string num1 = st.top();
     st.pop();
     std::string num2 = st.top();
     st.pop();
 
-    if(num1.find ('.') != std::string::npos || 
-            num2.find ('.') != std::string::npos )
-    {
-        double i = std::stod(num1);
-        double j = std::stod(num2);
-        double mul = i * j;
-        std::string res = std::to_string(mul);
-        st.push(res);
+    //Create a variable to store the result
+    std::string result;
 
-        std::cout << num1 << " * " << num2 << " = " 
-            << mul << std::endl;
+    //Check if either of the numbers are doubles
+    if(num1.find ('.') != std::string::npos || num2.find ('.') != std::string::npos ) {
+        //Multiply the two numbers together as doubles and push the result onto the stack
+        result = std::to_string(std::stod(num1) * std::stod(num2));
+        result = modify_string_precision(result); //set precision of result 
     }
+    //otherwise the numbers must be ints
     else {
-        int i = std::stoi(num1);
-        int j = std::stoi(num2);
-        int mul = i * j;
-        std::string res = std::to_string(mul);
-        st.push(res);
-
-        std::cout << num1 << " * " << num2 << " = " 
-            << mul << std::endl;
+        result = std::to_string(std::stoi(num1) * std::stoi(num2));
     }
+    //Push result to stack
+    st.push(result);
+
+    //Print out the equation
+    std::cout << num1 << " * " << num2 << " = " 
+        << result << std::endl; 
 }
+
 void divNumbers(std::stack<std::string>& st) {
+    //Pop off the top two variables from the stack
     std::string num1 = st.top();
     st.pop();
     std::string num2 = st.top();
     st.pop();
 
-    if(num1.find ('.') != std::string::npos || 
-            num2.find ('.') != std::string::npos )
-    {
-        double i = std::stod(num1);
-        double j = std::stod(num2);
-        double div = i / j;
-        std::string res = std::to_string(div);
-        st.push(res);
+    //Create a variable to store the result
+    std::string result;
 
-        std::cout << num1 << " / " << num2 << " = " 
-            << div << std::endl;
+    //Check if either of the numbers are doubles
+    if(num1.find ('.') != std::string::npos || num2.find ('.') != std::string::npos ) {
+
+        //Divide the two numbers together as doubles and push the result onto the stack
+        result = std::to_string(std::stod(num1) / std::stod(num2));
+        result = modify_string_precision(result); //set precision of result  
     }
+    //otherwise the numbers must be ints
     else {
-        int i = std::stoi(num1);
-        int j = std::stoi(num2);
-        int div = i / j;
-        std::string res = std::to_string(div);
-        st.push(res);
-
-        std::cout << num1 << " / " << num2 << " = " 
-            << div << std::endl;
+        result  = std::to_string(std::stoi(num1) / std::stoi(num2));
     }
+    //Push result onto the stack
+    st.push(result);
+    //Print out the equation
+    std::cout << num1 << " / " << num2 << " = " 
+        << result << std::endl;
 }
+
 void sqrtNumber(std::stack<std::string>& st) {
+    //Get top variable off stack
     std::string num = st.top();
     st.pop();
 
-    double i = std::stod(num);
-    double res = sqrt(i);
-    st.push(std::to_string(res));
+    std::string result;
 
-    std::cout << "sqrt " << i 
-        << " = " << res << std::endl;
+    //Check that the number is not negative
+    if(!(std::stoi(num) < 0)) {
+
+        if(num.find('.') != std::string::npos) {
+            result = std::to_string(sqrt(std::stod(num)));
+            result = modify_string_precision(result);
+        } else {
+            result = std::to_string(static_cast<int>(sqrt(std::stoi(num)))); //Need to static cast because sqrt returns a double value
+        }
+        //Push result onto stack
+        st.push(result);
+        std::cout << "sqrt " << num << " = " << result << std::endl;
+
+    } else {
+        std::cerr << "Cannot perform sqrt on invalid number. Number must be >= 0" << std::endl;
+    }
 }
+
+
 void reverseStack(std::stack<std::string>& st) {
     int n = std::stoi(st.top());
     st.pop();
 
+    //Create a temporary queue to store values we want to reverse
     std::queue<std::string> qu;
 
     for(int i = 0; i < n; ++i) {
         qu.push(st.top());
         st.pop();
     }
-
     while(!qu.empty()) {
         st.push(qu.front());
         qu.pop();
@@ -273,10 +289,11 @@ void printStack(std::stack<std::string>& st) {
     }
 }
 
-void printQueue(std::queue<std::string>& qu) {
-    while(!qu.empty()) {
-        std::string s = qu.front();
-        std::cout << s << " ";
-        qu.pop();
-    }
+std::string modify_string_precision(std::string a_value) {
+    double temp = std::stod(a_value);
+    std::ostringstream stream;
+    stream << std::fixed << std::setprecision(3) << temp;
+    std::string rand = stream.str();
+    return stream.str();
 }
+
